f901c14eff697168e828ef87fec485604cdb7479
Marius Brt <marius.brouty@gmail.com>
2022-09-08 20:38:34 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
 		<title>UM iCal</title>
 		<meta name="description" content="L'emploie du temps à l'UM en mode ez" />
 		<link rel="icon" href="/favicon.ico" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
 		<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"></meta>
 		<link href="/manifest.json" rel="manifest"></link>
 		
